3701dc19be145f9b2397790ef4a4ec05ff0fb9f2
devvikash18 <devvikash565@gmail.com>
2025-12-29 14:58:36 +0530

    added counter

--- Add-to-cart/Assets/js/script.js
+++ Add-to-cart/Assets/js/script.js
@@ -29,24 +29,37 @@ const products = [
   }
 ];
 
-function addToCart(proId){
-  console.log(proId)
+const cart = JSON.parse( localStorage.getItem("cart")) || []
+let counter = document.getElementById("counter")
 
+function updateCounter() {
+  counter.innerHTML = cart.length;
+}
+
+function addToCart(productId){
+  let product = products.find((pro) => {
+    return pro.id === productId;
+  })
+
+  cart.push(product)
+  localStorage.setItem("cart", JSON.stringify (cart))
+  updateCounter();
+  
 }
 
 let productRow = document.getElementById("product-row")
 
-products.forEach((pro, idx) => {
+products.forEach((product, idx) => {
   productRow.innerHTML += `
     <div class="col-xl-3 col-lg-4 col-md-6">
         <div class="product-grid-item card shadow">
           <div class="product-image">
-            <img src="${pro.image}" class="img-fluid" alt="">
+            <img src="${product.image}" class="img-fluid" alt="">
           </div>
             <div class="product-element text-start mx-3">
-              <p class="product-title fw-semibold"> ${pro.name}</p>
-                <p><strong>$ ${pro.price}.00</strong> </p>
-            <button class="btn btn-primary mb-3" onclick= "addToCart(${pro.id})">
+              <p class="product-title fw-semibold"> ${product.name}</p>
+                <p><strong>$ ${product.price}.00</strong> </p>
+            <button class="btn btn-primary mb-3" onclick= "addToCart(${product.id})">
                 Add to cart <i class="ri-shopping-cart-2-line"></i>
             </button>
           </div>
@@ -54,3 +67,5 @@ products.forEach((pro, idx) => {
      </div>
   `
 });
+
+updateCounter();
