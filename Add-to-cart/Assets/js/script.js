713d867ffb227476148b880420783bc6f2a809c5
devvikash18 <devvikash565@gmail.com>
2025-12-29 12:59:55 +0530

    removed some unnecessary classes

--- Add-to-cart/Assets/js/script.js
+++ Add-to-cart/Assets/js/script.js
@@ -2,28 +2,28 @@ const products = [
   {
     id: 1,
     name: "Vintage cinch backpack",
-    image: "./images/bag-1.webp",
+    image: "./Assets/images/bag-1.webp",
     price: 560,
     colors: ["#b87333", "#6a8f5f"]
   },
   {
     id: 2,
     name: "Nombined strapped backpack",
-    image: "./Assets/images/bag-1.webp",
+    image: "./Assets/images/bag-2.webp",
     price: 154,
     colors: ["#6a8f5f", "#333", "#b87333"]
   },
   {
     id: 3,
     name: "Lintage cinch backpack",
-    image: "./images/bag-3.jpg",
+    image: "./Assets/images/bag-3.jpg",
     price: 447,
     colors: []
   },
   {
     id: 4,
     name: "Jacquard ethnic backpack",
-    image: "./images/bag-4.webp",
+    image: "./Assets/images/bag-4.webp",
     price: 260,
     colors: ["#000", "#fff", "#b87333"]
   }
@@ -34,18 +34,18 @@ let productRow = document.getElementById("product-row")
 products.forEach((pro, idx) => {
   productRow.innerHTML += `
     <div class="col-6 col-sm-4 col-md-3">
-                    <div class="product-grid-item">
-                        <div class="product-image">
-                            <img src="${pro.image}" class="img-fluid" alt="">
-                        </div>
-                        <div class="product-element text-start mx-3">
-                            <p class="product-title fw-semibold"> ${pro.name}</p>
-                            <p>$${pro.price}</p>
-                            <button class="btn btn-primary">
-                                add to cart <i class="ri-shopping-cart-2-line"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
+        <div class="product-grid-item">
+          <div class="product-image">
+            <img src="${pro.image}" class="img-fluid" alt="">
+          </div>
+            <div class="product-element text-start mx-3">
+              <p class="product-title fw-semibold"> ${pro.name}</p>
+                <p><strong>$ ${pro.price}.00</strong> </p>
+            <button class="btn btn-primary">
+                Add to cart <i class="ri-shopping-cart-2-line"></i>
+            </button>
+          </div>
+       </div>
+     </div>
   `
 });
